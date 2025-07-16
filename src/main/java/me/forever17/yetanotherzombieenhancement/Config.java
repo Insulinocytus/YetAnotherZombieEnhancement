7b24a85b94b2d1efa4a0c1208cecece9641c1cef
Insulin <insulin@forever17.me>
2025-07-16 21:50:12 +0900

    update commnt by ai

--- src/main/java/me/forever17/yetanotherzombieenhancement/Config.java
+++ src/main/java/me/forever17/yetanotherzombieenhancement/Config.java
@@ -12,25 +12,29 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Neo's config APIs
+// 一个配置类的示例。这不是必需的，但最好有一个来保持你的配置井然有序。
+// 演示了如何使用 Neo 的配置 API
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     public static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
+            // 是否在通用设置中记录泥土方块
             .comment("Whether to log the dirt block on common setup")
             .define("logDirtBlock", true);
 
     public static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
+            // 一个魔法数字
             .comment("A magic number")
             .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
 
     public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
+            // 你希望魔法数字的介绍信息是什么
             .comment("What you want the introduction message to be for the magic number")
             .define("magicNumberIntroduction", "The magic number is... ");
 
-    // a list of strings that are treated as resource locations for items
+    // 一个字符串列表，被视为物品的资源位置
     public static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
+            // 在通用设置中记录的物品列表。
             .comment("A list of items to log on common setup.")
             .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), () -> "", Config::validateItemName);
 
--- src/main/java/me/forever17/yetanotherzombieenhancement/YetAnotherZombieEnhancementClient.java
+++ src/main/java/me/forever17/yetanotherzombieenhancement/YetAnotherZombieEnhancementClient.java
@@ -10,21 +10,21 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-// This class will not load on dedicated servers. Accessing client side code from here is safe.
+// 此类不会在专用服务器上加载。在此处访问客户端代码是安全的。
 @Mod(value = YetAnotherZombieEnhancement.MODID, dist = Dist.CLIENT)
-// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+// 您可以使用 EventBusSubscriber 自动注册类中所有使用 @SubscribeEvent 注解的静态方法
 @EventBusSubscriber(modid = YetAnotherZombieEnhancement.MODID, value = Dist.CLIENT)
 public class YetAnotherZombieEnhancementClient {
     public YetAnotherZombieEnhancementClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
+        // 允许 NeoForge 为此 mod 的配置创建配置屏幕。
+        // 通过进入“Mods”屏幕 > 单击您的 mod > 单击“config”来访问配置屏幕。
+        // 不要忘记将您的配置选项的翻译添加到 en_us.json 文件中。
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
+        // 一些客户端设置代码
         YetAnotherZombieEnhancement.LOGGER.info("HELLO FROM CLIENT SETUP");
         YetAnotherZombieEnhancement.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
